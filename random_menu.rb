99bbbae27c32a5b4236d6900902c9519e2c1eedb
Loraine <loraine@lorainekv.com>
2015-05-17 00:26:48 -0700

    Added a few more adjectives.

--- random_menu.rb
+++ random_menu.rb
@@ -2,13 +2,13 @@
 # Thanks Pinterest for the inspiration
 
 #Creates an adjective array
-adjs = ["salty", "buttery", "spicy", "creamy", "frosty", "bitter", "sour", "sweet", "chunky", "hot", "cheesy"]
+adjs = ["salty", "buttery", "spicy", "creamy", "frosty", "bitter", "sour", "sweet", "chunky", "hot", "cheesy", "burnt", "candied", "drizzled", "fluffy"]
 
 #Creates a booze array
-booze = ["champagne", "stout", "rum", "vodka", "moonshine", "Kahlua", "bourbon", "red wine", "Bacardi 151", "tequila", "malt liquor"]
+booze = ["champagne", "stout", "rum", "vodka", "moonshine", "Kahlua", "bourbon", "red wine", "Bacardi 151", "tequila", "malt liquor", "Amaretto", "peach schnapps", "cider", "Fireball"]
 
 #Creates a dessert array
-dessert = ["coffee", "shake", "ice cream", "brownie", "wafffle", "cake", "jello", "cookie", "doughnut", "pudding", "eclairs" ]
+dessert = ["coffee", "shake", "ice cream", "brownie", "waffle", "cake", "jello", "cookie", "doughnut", "pudding", "eclair", "cream puff", "pastry", "pie", "candy" ]
 
 #Loop to print numbered list
 list_num = 1
